b63e7166232fb742ff8871c4876b2b0ee21c6582
Ben Jones <benjones012105@gmail.com>
2025-08-07 16:16:23 +0100

    Dataset and preprocessing

--- preprocessing/fetch_binance.py
+++ preprocessing/fetch_binance.py
@@ -1,8 +1,27 @@
 import ccxt 
 import pandas as pd
 import time
+import os
+from datetime import datetime, timedelta
 
-def fetch_binance(exchange_ticker = 'BTC/USDT',start_date = '2019-01-01T00:00:00Z',timeframe = '1h'):
+def fetch_binance(
+    exchange_ticker = 'BTC/USDT',
+    start_date = '2019-01-01T00:00:00Z',
+    timeframe = '1h',
+    cache_path = './data/binance_btc_usdt.csv',
+    max_age_hrs = 12
+    ):
+    
+    def path_age_check(cache_path, max_age_hrs):
+        last_mod = datetime.fromtimestamp(os.path.getmtime(cache_path))
+        age = datetime.now() - last_mod
+        return age < timedelta(hours = max_age_hrs)
+    
+    if os.path.exists(cache_path) and path_age_check(cache_path,max_age_hrs):
+            print(f'Loading prexisting data from {cache_path}')
+            data_df = pd.read_csv(cache_path,index_col='timestamp', parse_dates= True)
+            return data_df
+    
     binance = ccxt.binance()
     now = binance.milliseconds()
     since = binance.parse8601(start_date) 
@@ -19,14 +38,16 @@ def fetch_binance(exchange_ticker = 'BTC/USDT',start_date = '2019-01-01T00:00:00
         last_time = ohlcv[-1][0] #OHLCV is a list of ms stamp,open,...,vol draws correct stamp
         since = last_time + 1 #dodge duplicate data
         
-        time.sleep(binance.rateLimit / 1000)
+        time.sleep(binance.rateLimit / 1000) #Ensures it doesnt hit rate lim and converts to s for sleep
         
     data_df = pd.DataFrame.from_records(
         data,
         columns= ['timestamp', 'open', 'high', 'low', 'close', 'volume']
     )
-    data_df['timestamp'] = pd.to_datetime(data_df['timestamp'])
+    data_df['timestamp'] = pd.to_datetime(data_df['timestamp'], unit = 'ms')
     data_df.set_index('timestamp',inplace= True)
-
-    return data_df
     
+    os.makedirs(os.path.dirname(cache_path), exist_ok= True)
+    data_df.to_csv(cache_path)
+    
+    return data_df
---
+++ preprocessing/prep_data.py
@@ -0,0 +1,48 @@
+import torch 
+import pandas as pd
+import numpy as np
+
+def target_def(df):
+    daily_change = df['close'].shift(-1) - df['close']
+    df['label'] = np.where(daily_change > 0,1,0)
+    df = df[:-1]
+    return df 
+
+def z_score_norm(df,split_time = '2023-01-01 00:00:00'):
+    norm_df = df.drop('label',axis = 1)
+    test = norm_df.loc[:'2023-01-01 00:00:00']
+    mean = test.mean()
+    std = test.std()
+    norm_df = (norm_df - mean) / std
+    norm_df['label'] = df['label']
+    return norm_df
+
+def window_creation(norm_df,window_size = 48):
+    features = norm_df.drop('label',axis = 1)
+    labels = norm_df['label']
+    
+    x = []
+    y = []
+    for i in range(len(labels)-window_size):
+        x.append(features[i:i + window_size])
+        y.append(labels[i+window_size])
+        
+    x = np.array(x)
+    y = np.array(y)
+    
+    x_tensor = torch.tensor(x, dtype= torch.float32)
+    y_tensor = torch.tensor(y, dtype= torch.float32)
+
+    return x_tensor,y_tensor
+
+def train_val_test_split (x,y,train_frac = 0.7,val_frac = 0.15):
+    size = len(y)
+    train_end = size * train_frac
+    val_end = size * (train_frac + val_frac)
+    
+    x_train,y_train = x[:train_end],y[:train_end]
+    x_val, y_val = x[train_end:val_end],y[train_end:val_end]
+    x_test,y_test = x[val_end:],y[val_end]
+    
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
